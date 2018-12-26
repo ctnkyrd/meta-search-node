7ea8a607173606b8e17524ef0164edb9c703d998
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-12-26 13:48:02 +0300

    change IP/PORT

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.use("/", indexRoutes);
 
 
 
-// const PORT = process.env.PORT;
-// const IP = process.env.IP;
+const PORT = process.env.PORT;
+const IP = process.env.IP;
 
-const PORT =  3000;
-const IP = 'localhost';
+// const PORT =  3000;
+// const IP = 'localhost';
 
 app.listen(PORT, IP, function(){
     console.log("Server Started!", IP+":"+PORT);
