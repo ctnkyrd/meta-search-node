8732f5861e05b2dd64f84e4856bb1dd8d007d22c
root <ctnkyrd@gmail.com>
2019-03-20 11:15:13 +0300

    add ssl support and styling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var express           = require("express"),
     app               = express();
 
@@ -5,15 +6,36 @@ var indexRoutes       = require("./routes/index");
 
     
 app.set("view engine", "ejs");
+app.set('views', __dirname+'/views');
 app.use(express.static(__dirname + "/public"));
+
 app.use("/", indexRoutes);
 
-// const PORT = process.env.PORT;
-// const IP = process.env.IP;
-
+/*
 const PORT =  5000;
-const IP = '192.168.1.190';
+const IP = '172.17.32.184';
 
 app.listen(PORT, IP, function(){
     console.log("Server Started!", IP+":"+PORT);
- });
+ }); */
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+//var privateKey  = fs.readFileSync(__dirname+'/ssl/csb_2019.key', 'utf8');
+//var certificate = fs.readFileSync(__dirname+'/ssl/csb2.crt', 'utf8');
+
+var privateKey  = fs.readFileSync('/var/lib/ssl/csb_2019.key', 'utf8');
+var certificate = fs.readFileSync('/var/lib/ssl/csb2.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(5000);
+httpsServer.listen(443);
+
